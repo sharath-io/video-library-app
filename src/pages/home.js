8c20ee152ebd7cc34b9ed9297eb59e72dc5f0ffd
Sharath <sharath.esr@gmail.com>
2023-05-10 13:45:49 +0530

    home page is done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,10 @@
+import {NavLink} from 'react-router-dom';
 export function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h2>welcome to video library</h2>
+            <h2>To browse all videos, click below button or go to videos page</h2>
+            <NavLink to="/videos" className="nav-link"><button>Explore videos</button></NavLink>
         </div>
     )
 }
