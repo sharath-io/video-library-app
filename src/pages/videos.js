52e11e52f2b9f001f0f03e7020f7dea489951ae8
Sharath <sharath.esr@gmail.com>
2023-05-11 07:07:41 +0530

    added remove like in videos page

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -7,7 +7,7 @@ import { WatchlaterContext } from "../contexts/watchlaterContext";
 export function Videos() {
     const {videosData} = useContext(VideoContext);
     const {addToWatchlater,watchlaterData } = useContext(WatchlaterContext);
-    const {addToLike,likesData} = useContext(LikeContext)
+    const {addToLike,likesData,removeLike} = useContext(LikeContext)
     return (
         <div>
             <h2>All Videos</h2>
@@ -17,7 +17,11 @@ export function Videos() {
                videosData.map((video) =>  <li key={video.id} className="video-card">
                 <img src={video.thumbnail} alt={video.title}/>
                 <p>{video.title}</p>
-                <button onClick={()=> addToLike(video.id)} disabled={likesData.includes(video) ? true : false}>Like</button>
+                {!likesData.includes(video) 
+                ? <button onClick={()=> addToLike(video.id)}>Like</button>
+                : <button onClick={()=> removeLike(video.id)}>Remove Like</button>
+                }
+
                 {!watchlaterData.includes(video) 
                 ?  <button onClick={()=> addToWatchlater(video.id)}>Add to Watch Later</button>
                 : <NavLink to="/watchlater"><button>Go to Watch Later</button></NavLink>
